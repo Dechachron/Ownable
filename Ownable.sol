5131861b73b488e2e46da0604e4b4039ba7be8d5
DCN <116252572+Dechachron@users.noreply.github.com>
2022-12-08 13:14:51 +0530

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7; 
   
- import "../utils/Context.sol"; 
+ import "github.com/Dechachron/Abstract/blob/main/abstract.sol"; 
   
  /** 
   * @dev Contract module which provides a basic access control mechanism, where 
