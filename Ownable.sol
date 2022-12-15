f6a9db320774c8d0d0fa114e143c6fbf7a91aea8
DCN <116252572+Dechachron@users.noreply.github.com>
2022-12-15 12:25:30 +0530

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.7;
 
 import "github.com/Dechachron/Abstract/blob/main/abstract.sol";
 
- abstract contract Ownable is Context {
+contract Ownable is Context {
     address private _owner;
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     constructor () {
