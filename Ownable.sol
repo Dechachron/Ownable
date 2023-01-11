651884b722c4f22af15509f2f18dcf67ac9a2747
DCN <116252572+Dechachron@users.noreply.github.com>
2023-01-11 18:35:51 +0530

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
-pragma solidity 0.6.12;
-
-import "github.com/Dechachron/Context/blob/main/Contextt.sol";
+pragma solidity 0.8.10;
 
 contract Ownable is Context {
     address private _owner;
